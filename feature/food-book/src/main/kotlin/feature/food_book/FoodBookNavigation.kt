5366d0cbeab5db9cfdbd91490b39f7d33c7d1baf
whitipet <whitipet@gmail.com>
2024-06-02 18:17:57 +0300

    Add FoodBook runConfig

--- feature/food-book/src/main/kotlin/feature/food_book/FoodBookNavigation.kt
+++ feature/food-book/src/main/kotlin/feature/food_book/FoodBookNavigation.kt
@@ -6,6 +6,7 @@ import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
+import androidx.navigation.navDeepLink
 import org.koin.androidx.compose.navigation.koinNavViewModel
 
 private const val FoodBookRoute = "food_book"
@@ -15,7 +16,12 @@ fun NavGraphBuilder.foodBookScreen(
 	onCloseScreenAction: (route: String) -> Unit = {},
 	onAddAction: () -> Unit = {},
 	sharedTransitionScope: SharedTransitionScope,
-) = composable(FoodBookRoute) {
+) = composable(
+	route = FoodBookRoute,
+	deepLinks = listOf(navDeepLink {
+		uriPattern = "ec://${FoodBookRoute}"
+	})
+) {
 	val vm: FoodBookViewModel = koinNavViewModel()
 	FoodBookScreen(
 		uiState = vm.uiState.collectAsStateWithLifecycle(),
